67ccf28afb6a33604d15a3eba48dcdf0599c3d99
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-10 17:11:40 -0600

    Adding mailto y href tel

--- js/main.js
+++ js/main.js
@@ -25,21 +25,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-const email = document.getElementById('go-email');
-email.addEventListener('click', (event) =>{
-        swal({
-            title: "Correo Electrónico",
-            text: "garcia_romero.y@hotmail.com",
-            button: "Escribeme",
-        });
-});
-
-const celular = document.getElementById('go-phone');
-celular.addEventListener('click', (event) =>{
-        swal({
-            title: "Télefono celular",
-            text: "5527258173",
-            button: "Escribeme",
-        });
-});
