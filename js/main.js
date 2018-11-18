61ffa0b0a04098ff12e791b9c63f209a1dccb562
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-17 23:06:00 -0600

    Adding the test files, fixing htmlhint problems and resolvinng eslint problems, kepping eslint problems related with scrollreveral npm library

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-window.sr = ScrollReveal();
+window.sr = ScrollReveal({reset: 'true'});
 // Animations for the boxes into about me section
 sr.reveal('.about-me-left', {
-    duration: 2000,
-    origin: 'left',
-    distance: '300px'
+	duration: 2000,
+	origin: 'left',
+	distance: '300px'
 });
 sr.reveal('.about-me-center', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '300px'
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px'
 });
 sr.reveal('.about-me-right', {
-    duration: 2000,
-    origin: 'right',
-    distance: '300px'
+	duration: 2000,
+	origin: 'right',
+	distance: '300px'
 });
-/*Smooth scrolling effects */
+/* Smooth scrolling effects */
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
 });
---
+++ test/main.spec.js
@@ -0,0 +1,5 @@
+describe('main', () =>{
+    it('debería exponer función ScrollReveal en objeto window', () => {
+    assert.isFunction(ScrollReveal);
+    });
+});
