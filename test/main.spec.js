1dfece4d74588b24f7364bbc32ad13d33c3060bc
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-25 14:38:09 -0600

    Incrementing test coverage to 33.3%

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,16 @@
+var chai = require('chai');
+var assert = chai.assert;
+var describe = chai.describe;
+var it = chai.it;
+
 describe('main', () =>{
-    it('debería exponer función ScrollReveal en objeto window', () => {
-    assert.isFunction(ScrollReveal);
-    });
+	it('should return -1 when the value is not present', function () {
+		assert.isFunction(aboutLeft, 'great, is function');
+	});
+	it('should return -1 when the value is not present', function () {
+		assert.isFunction(aboutCenter, 'great, is function');
+	});
+	it('should return -1 when the value is not present', function () {
+		assert.isFunction(aboutR, 'great, is function');
+	});
 });
